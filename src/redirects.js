8d7c18dadbe069c13090ec8073426f2f6cfa5780
ADG <2007andredoe@gmail.com>
2022-07-14 12:01:03 -0300

    Update redirects.js

--- src/redirects.js
+++ src/redirects.js
@@ -3,6 +3,6 @@ module.exports = (app) => {
 		res.redirect('https://discord.com/oauth2/authorize?client_id=880173509077266483&permissions=268823622&redirect_uri=http%3A%2F%2Fzulybot.xyz%2F&scope=bot%20applications.commands');
 	});
 	app.get('/discord', (req, res) => {
-		res.redirect('https://discord.gg/pyyyJpw5QW');
+		res.redirect('https://discord.gg/8SA5sfyR7g');
 	});
 };
